35f56a2064ba2adb554ed40513f2ea18596c79aa
PrimeraPostura <joel.pino02@inacapmail.cl>
2024-12-02 21:40:37 -0300

    Segundo commit

--- lib/screens/my_wines_screen.dart
+++ lib/screens/my_wines_screen.dart
@@ -8,22 +8,6 @@ class MyWinesScreen extends StatelessWidget {
   // Obtener el ID del usuario actual
   User? get currentUser => FirebaseAuth.instance.currentUser;
 
-  // Recupera los vinos del usuario en Firestore
-  Future<List<Map<String, dynamic>>> _getUserWines() async {
-    final snapshot = await FirebaseFirestore.instance
-        .collection('user')
-        .doc(currentUser?.uid)
-        .collection('wines')
-        .get();
-
-    return snapshot.docs
-        .map((doc) => {
-              'id': doc.id, // Asegúrate de obtener el ID del documento
-              'data': doc.data() as Map<String, dynamic>
-            })
-        .toList();
-  }
-
   // Método para eliminar un vino
   Future<void> _deleteWine(BuildContext context, String wineId) async {
     try {
@@ -45,8 +29,13 @@ class MyWinesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<Map<String, dynamic>>>(
-      future: _getUserWines(),
+    return StreamBuilder<QuerySnapshot>(
+      // Utilizamos un stream para escuchar cambios en tiempo real
+      stream: FirebaseFirestore.instance
+          .collection('user')
+          .doc(currentUser?.uid)
+          .collection('wines')
+          .snapshots(), // Se escucha en tiempo real
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const Center(child: CircularProgressIndicator());
@@ -54,21 +43,20 @@ class MyWinesScreen extends StatelessWidget {
         if (snapshot.hasError) {
           return Center(child: Text('Error: ${snapshot.error}'));
         }
-        if (!snapshot.hasData || snapshot.data!.isEmpty) {
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
           return const Center(child: Text('No tienes vinos en tu bodega'));
         }
 
-        final wines = snapshot.data!;
+        final wines = snapshot.data!.docs;
 
         return ListView.builder(
           itemCount: wines.length,
           itemBuilder: (context, index) {
-            final wine = wines[index];
-            final wineData = wine['data']; // Obtener los datos del vino
-            final wineId = wine['id']; // Obtener el ID del vino
+            final wineData = wines[index].data() as Map<String, dynamic>;
+            final wineId = wines[index].id; // ID del vino
 
             return ListTile(
-              leading: Icon(Icons.local_bar), // O imagen aquí
+              leading: const Icon(Icons.local_bar), // O una imagen aquí
               title: Text(wineData['nombre']),
               subtitle: Text('Año: ${wineData['año_cosecha']}'),
               trailing: IconButton(
